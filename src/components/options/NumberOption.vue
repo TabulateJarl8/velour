8b3f8c233eae27d60615d98fc9eca4be52d0fd42
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-02-25 01:35:27 -0500

    disable num input tickers because theyre being annoying

--- src/components/options/NumberOption.vue
+++ src/components/options/NumberOption.vue
@@ -12,10 +12,12 @@ defineProps<{
     <span class="label-text font-semibold">{{ opt.label }}</span>
   </label>
   <label class="label cursor-pointer justify-start gap-4">
+    <!-- set the width to either the max length, placeholder length, or default to 3 -->
     <input
       type="number"
       required
-      class="input input-sm validator"
+      class="input input-sm validator text-center"
+      :style="{ width: `${String(opt.max || opt.placeholder || 999).length + 5}ch` }"
       v-model="model"
       :placeholder="opt['placeholder']"
       :min="opt['min']"
