359697f986714f384b89f64a0b04b4c2ee1b6ed2
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-03-03 12:21:56 -0500

    remove commented out code and wiki comments

--- src/components/options/DropdownOption.vue
+++ src/components/options/DropdownOption.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { DropdownSubOption } from '@/core/types'
 
-// the type here needs to match the type in SubOptionTypeMap
 const model = defineModel<string>({ required: true })
 defineProps<{
   opt: DropdownSubOption
@@ -12,15 +11,11 @@ defineProps<{
   <label class="label cursor-pointer gap-3 items-center mt-2 w-full">
     <div class="flex flex-col flex-1 hover:text-white transition-colors">
       <span class="label-text font-semibold">{{ opt.label }}</span>
-      <span
-        v-if="opt.description"
-        class="label-text text-xs opacity-70 leading-4 whitespace-normal"
-      >
+      <span v-if="opt.description" class="label-text text-xs opacity-70 leading-4 whitespace-normal">
         {{ opt.description }}
       </span>
     </div>
 
-    <!-- this is the main thing that needs to be modified -->
     <select class="select select-sm select-bordered" v-model="model">
       <option disabled value="">Select an option</option>
       <option v-for="choice in opt.options" :key="choice.value" :value="choice.value">
