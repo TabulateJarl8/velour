65d11f0f08ed70841f1f0400cd0193be8206f298
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-03-03 18:55:09 -0500

    Revert "prefer system shellcheck over downloaded one"
    This reverts commit 05f0c87de6b675ff388069da0491fe1102c1aeda.
    
    It made things slower when the system shellcheck is dynamically linked

--- tests/vitest.global.ts
+++ tests/vitest.global.ts
@@ -1,10 +1,6 @@
 import { shellcheck } from 'shellcheck'
-import which from 'which'
 
 /** Install shellcheck before any tests run */
 export default async function setup() {
-  const path = await which('shellcheck', { nothrow: true })
-
-  // download shellcheck if host system doesnt have it installed
-  if (!path) await shellcheck({ args: ['--version'] })
+  await shellcheck({ args: ['--version'] })
 }
--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,28 +1,10 @@
 import { config as shellcheckConfig } from 'shellcheck/build/configs/config.js'
 import { spawnSync } from 'node:child_process'
 import { expect } from 'vitest'
-import which from 'which'
-
-let shellcheckPath: string | null = null
-
-const getShellcheckPath = () => {
-  if (shellcheckPath) return shellcheckPath
-
-  const path = which.sync('shellcheck', { nothrow: true })
-  if (path) {
-    shellcheckPath = path
-    return shellcheckPath
-  }
-
-  // host system doesnt have shellcheck installed, use the locally downloaded one
-  shellcheckPath = shellcheckConfig.bin
-  return shellcheckPath
-}
 
 expect.extend({
   toBeValidBash(content: string) {
-    const shellcheck = getShellcheckPath()
-    const result = spawnSync(shellcheck, ['-s', 'bash', '-'], {
+    const result = spawnSync(shellcheckConfig.bin, ['-s', 'bash', '-'], {
       input: content,
       encoding: 'utf-8',
     })
