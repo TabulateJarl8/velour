05f0c87de6b675ff388069da0491fe1102c1aeda
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-03-03 18:37:31 -0500

    prefer system shellcheck over downloaded one

--- tests/vitest.global.ts
+++ tests/vitest.global.ts
@@ -1,6 +1,10 @@
 import { shellcheck } from 'shellcheck'
+import which from 'which'
 
 /** Install shellcheck before any tests run */
 export default async function setup() {
-  await shellcheck({ args: ['--version'] })
+  const path = await which('shellcheck', { nothrow: true })
+
+  // download shellcheck if host system doesnt have it installed
+  if (!path) await shellcheck({ args: ['--version'] })
 }
