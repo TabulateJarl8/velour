3415970b9ef796f5cb5289196323533aa996583a
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-03-03 12:23:35 -0500

    i really need to figure out how to centralize vscode formatting options

--- src/components/options/DropdownOption.vue
+++ src/components/options/DropdownOption.vue
@@ -11,7 +11,10 @@ defineProps<{
   <label class="label cursor-pointer gap-3 items-center mt-2 w-full">
     <div class="flex flex-col flex-1 hover:text-white transition-colors">
       <span class="label-text font-semibold">{{ opt.label }}</span>
-      <span v-if="opt.description" class="label-text text-xs opacity-70 leading-4 whitespace-normal">
+      <span
+        v-if="opt.description"
+        class="label-text text-xs opacity-70 leading-4 whitespace-normal"
+      >
         {{ opt.description }}
       </span>
     </div>
--- src/components/options/RadioOption.vue
+++ src/components/options/RadioOption.vue
@@ -11,16 +11,27 @@ defineProps<{
   <label class="label cursor-pointer gap-3 items-center mt-2 w-full">
     <div class="flex flex-col flex-1">
       <span class="label-text font-semibold">{{ opt.label }}</span>
-      <span v-if="opt.description" class="label-text text-xs opacity-70 leading-4 whitespace-normal">
+      <span
+        v-if="opt.description"
+        class="label-text text-xs opacity-70 leading-4 whitespace-normal"
+      >
         {{ opt.description }}
       </span>
     </div>
   </label>
 
   <div class="flex flex-col gap-1.5 p-2">
-    <label v-for="choice in opt.options" :key="choice.value"
-      class="label cursor-pointer justify-start gap-4 hover:text-white transition-colors">
-      <input type="radio" class="radio radio-sm radio-secondary" v-model="model" :value="choice.value" />
+    <label
+      v-for="choice in opt.options"
+      :key="choice.value"
+      class="label cursor-pointer justify-start gap-4 hover:text-white transition-colors"
+    >
+      <input
+        type="radio"
+        class="radio radio-sm radio-secondary"
+        v-model="model"
+        :value="choice.value"
+      />
       <span class="label-text">{{ choice.label }}</span>
     </label>
   </div>
