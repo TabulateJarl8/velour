60dd7bd4903978b74a82964d072d57e348bf2115
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-03-06 00:57:03 -0500

    implement simple alert cards for plugins
    closes #31

--- src/components/PluginOptionsCard.vue
+++ src/components/PluginOptionsCard.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import type { ConcretePluginConfig, ConcretePluginDef } from '@/core/types'
+import type { ConcretePluginConfig, ConcretePluginDef, PluginAlert } from '@/core/types'
 import CheckboxOption from './options/CheckboxOption.vue'
 import TextOption from './options/TextOption.vue'
 import NumberOption from './options/NumberOption.vue'
 import RadioOption from './options/RadioOption.vue'
 import DropdownOption from './options/DropdownOption.vue'
+import { computed } from 'vue'
 
 const model = defineModel<ConcretePluginConfig>({ required: true })
 const componentMap: Record<string, unknown> = {
@@ -15,9 +16,21 @@ const componentMap: Record<string, unknown> = {
   dropdown: DropdownOption,
 }
 
-defineProps<{
+const props = defineProps<{
   plugin: ConcretePluginDef
 }>()
+
+const alert = computed<PluginAlert | undefined>(() => {
+  return props.plugin.alerts ? props.plugin.alerts(model.value) : undefined
+})
+
+// workaround so that tailwind includes the class names
+const alertClasses: Record<string, string> = {
+  info: 'alert-info',
+  warning: 'alert-warning',
+  success: 'alert-success',
+  error: 'alert-error',
+}
 </script>
 
 <template>
@@ -46,6 +59,15 @@ defineProps<{
             :opt="opt"
           />
         </div>
+      </div>
+
+      <div
+        v-if="alert"
+        role="alert"
+        class="alert alert-soft mt-3"
+        :class="alertClasses[alert.type]"
+      >
+        <span>{{ alert.message }}</span>
       </div>
     </div>
   </div>
