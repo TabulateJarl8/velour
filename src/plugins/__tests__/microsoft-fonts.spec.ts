e332bc726d69bbf1a9ffca1abc57b054e0f759e6
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-04-02 00:47:21 -0400

    plugin unit tests

---
+++ src/plugins/__tests__/microsoft-fonts.spec.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest'
+import plugin from '../customization/microsoft-fonts'
+
+describe('microsoft-fonts', () => {
+  it('warns the user when official windows fonts are selected', () => {
+    const config = { enabled: true, installType: 'windows' }
+    const alertMessage = plugin.alerts?.(config)
+    expect(alertMessage?.type).toBe('warning')
+    expect(alertMessage?.message).not.toHaveLength(0)
+  })
+
+  it('doesnt warn the user when core ms fonts are selected', () => {
+    const config = { enabled: true, installType: 'core' }
+    const alertMessage = plugin.alerts?.(config)
+    expect(alertMessage).toBeUndefined()
+  })
+
+  it('catch invalid installTypes', () => {
+    const config = { enabled: true, installType: 'bad' }
+    const script = plugin.generate(config)
+
+    expect(script).toContain('# (Microsoft Fonts) INVALID INSTALLATION TYPE: bad')
+  })
+})
---
+++ src/plugins/__tests__/set-hostname.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest'
+import plugin from '../system/set-hostname'
+
+describe('set-hostname', () => {
+  it('rejects hostnames with invalid characters', () => {
+    const badHostnames = ['WOW', 'w*4', '-wee']
+
+    for (const bad of badHostnames) {
+      expect(plugin.options.hostname.validate(bad)).toBeTypeOf('string')
+    }
+  })
+
+  it('rejects hostnames that are too long', () => {
+    const bad = 's'.repeat(255)
+    expect(plugin.options.hostname.validate(bad)).toBeTypeOf('string')
+  })
+
+  it('rejects hostnames with parts that are too long', () => {
+    const bad = 's'.repeat(64) + '.' + 's'
+    expect(plugin.options.hostname.validate(bad)).toBeTypeOf('string')
+  })
+
+  it('accepts valid hostnames', () => {
+    const hostnames = [
+      'marbles',
+      'xeep-4',
+      'super.com.edu',
+      's'.repeat(62) + '.' + 'femeral-artery',
+    ]
+
+    for (const hn of hostnames) {
+      expect(plugin.options.hostname.validate(hn)).toBeUndefined()
+    }
+  })
+})
